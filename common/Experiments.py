bd51182fca954b74800b4496b257f9feba8e70b9
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-18 21:19:45 +0530

    Experiment Class

--- common/Experiments.py
+++ common/Experiments.py
@@ -6,39 +6,50 @@ import flwr as fl
 
 # Let us initialize all the parameters that we will be using for testing
 
-valid_fusions = []
-valid_models = []
-valid_datasets = []
-valid_batch_sizes : list[int] = [16, 512]
-valid_sample_fractions : list[float] = [1.0]
-
-valid_rounds : list[int] = [3]
-valid_epochs : list[int] = [4]
-valid_rounds_and_epochs = [
-    # Rounds, Epochs
-    (3,4)
-]
+valid_fusions = ['FedAvg']
+valid_models = ['tf-cnn']
+valid_datasets = ['cifar10', 'mnist']
 
 # Initializing params that are not required for every experiment
 
-valid_proximal_mu : list[float] = [1.0]
+valid_proximal_mu: list[float] = [1.0]
 
-class Experiment():
 
+class Experiment:
     fusion_translator = {
-        'FedAvg' : fl.server.strategy.FedAvg,
-        'FedProx' : fl.server.strategy.FedProx,
+        "FedAvg": fl.server.strategy.FedAvg,
+        "FedProx": fl.server.strategy.FedProx,
     }
-    
-    def __init__(self, model : str, fusion : str, dataset : str, batch_size: int, sample_fraction : float, proximal_mu : float, rounds : int, epochs : int) -> None:
+
+    def __init__(
+        self,
+        model: str,
+        fusion: str,
+        dataset: str,
+        batch_size: int,
+        rounds: int,
+        epochs: int,
+        proximal_mu: float | None,
+        sample_fraction: float = 1.0,
+    ) -> None:
         self.model = model
         self.fusion = fusion
         self.dataset = dataset
         self.batch_size = batch_size
-        self.sample_fraction = sample_fraction
-        self.proximal_mu = proximal_mu
         self.rounds = rounds
         self.epochs = epochs
-        
+        self.sample_fraction = sample_fraction
+        self.proximal_mu = proximal_mu
+        self.check_validity()
 
-
+    def check_validity(self):
+        if self.model not in valid_models:
+            raise ValueError(f"{self.model} is not a valid model")
+        if self.fusion not in valid_fusions:
+            raise ValueError(f"{self.fusion} is not a valid fusion")
+        if self.dataset not in valid_models:
+            raise ValueError(f"{self.dataset} is not a valid dataset")
+        if not (0.0 < self.sample_fraction <= 1.0):
+            raise ValueError(f"{self.sample_fraction} is not a valid sample-fraction")
+        if not (0 <= self.proximal_mu):
+            raise ValueError(f"{self.proximal_mu} is not a valid proximal-mu")
