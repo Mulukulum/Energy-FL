abaecbe65661916560cb0191b7ee145e40e6dce3
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-18 21:38:21 +0530

    Party class

--- Clients/Aggregator.py
+++ Clients/Aggregator.py
@@ -1,10 +1,9 @@
-class Aggregator():
-    
-    def __init__(self, ip: str, username: str, flwrPort: int, zmqPort : int) -> None:
+class Aggregator:
+    def __init__(self, ip: str, username: str, flwrPort: int, zmqPort: int) -> None:
         self.ip = ip
         self.username = username
         self.flwrPort = flwrPort
         self.zmqPort = zmqPort
-        
+
     def __repr__(self) -> str:
-        return f"""Aggregator on IP {self.ip}"""
+        return f"""Aggregator {self.username}@{self.ip} hosting flwr on {self.flwrPort} and broadcasting on {self.zmqPort}"""
---
+++ Clients/Party.py
@@ -0,0 +1,7 @@
+
+class Party:
+    
+    def __init__(self, ip: str, username: str) -> None:
+        self.ip = ip
+        self.username = username
+        
