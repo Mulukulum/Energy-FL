56df2b1f77db56fee0ec7f71c425fa921e1faf9d
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-19 22:50:37 +0530

    Started work on SAR

--- Clients/Aggregator.py
+++ Clients/Aggregator.py
@@ -117,3 +117,11 @@ class Aggregator:
         
         return list_of_experiments
     
+"""
+#!/bin/bash
+color='\033[1;31m' # set the color to bold red
+
+hosts=("10.8.1.45" "10.8.1.38" "10.8.1.43" "10.8.1.192" "10.8.1.41")
+
+usernames=("user" "rpi1" "rpi2" "rpi3" "rpi4")
+"""
---
+++ Clients/Scripts/sar_collector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+color='\033[1;31m' # set the color to bold red
+
+hosts=("10.8.1.45" "10.8.1.38" "10.8.1.43" "10.8.1.192" "10.8.1.41")
+
+usernames=("user" "rpi1" "rpi2" "rpi3" "rpi4")
+
