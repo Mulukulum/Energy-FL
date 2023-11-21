60cde4ba7e1f4a58c07daa173e844f65c40b0a9a
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-21 17:14:07 +0530

    SAR-Cleanup

--- Clients/Scripts/sar_collector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-color='\033[1;31m' # set the color to bold red
-
-hosts=("10.8.1.45" "10.8.1.38" "10.8.1.43" "10.8.1.192" "10.8.1.41")
-
-usernames=("user" "rpi1" "rpi2" "rpi3" "rpi4")
-
