1f9b619d29486efccb38d00524359b2a8bc630a1
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-23 20:35:06 +0530

    Revert "Delete Archive Folder"
    This reverts commit a15b9b8e6a76f422d10f6425b5fcd1dafa5618c4.

---
+++ Functions/Archive/clear_cache_port.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Killing process at the given ports
+#sudo kill -9 $(sudo lsof -t -i :$1)
+
+#Clearing cache
+#sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+
+#Clear swap
+#sudo swapoff -a && sudo swapon -a
