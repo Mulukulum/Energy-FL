352beb5cc7c9393ff6969985acc1fc7ba993be8a
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-25 16:20:54 +0530

    Update Configuration.py

--- common/Configuration.py
+++ common/Configuration.py
@@ -1,27 +1,18 @@
-
 # ! This file contains the device configurations for the experiments
 
 # * Stuff like the IPs of the devices, ZMQ Ports and Which device is collecting power for which other device etc is here
 
 IP_CLIENTS = {
-    "rpi1" : "10.8.1.38",
-    "rpi2" : "10.8.1.43",
-    "rpi3" : "10.8.1.192",
-    "rpi4" : "10.8.1.41"
+    "rpi1": "10.8.1.38",
+    "rpi2": "10.8.1.43",
+    "rpi3": "10.8.1.192",
+    "rpi4": "10.8.1.41",
 }
 
-IP_POWER_COLLECTORS = {
-    "pi3" : "10.8.1.200"
-}
+AGGREGATOR_ZMQ_BROADCAST_PORT = 6010
 
-UM25C_ADDR_FOR_POWER_COLLECTORS = {
-    "pi3" : "98:DA:F0:00:4A:13"
-}
+IP_POWER_COLLECTORS = {"pi3": "10.8.1.200"}
 
-POWER_COLLECTOR_CONNECTED_DEVICE = {
-    "pi3" : "rpi3"
-}
+UM25C_ADDR_FOR_POWER_COLLECTORS = {"pi3": "98:DA:F0:00:4A:13"}
 
-
-
-
+POWER_COLLECTOR_CONNECTED_DEVICE = {"pi3": "rpi3"}
