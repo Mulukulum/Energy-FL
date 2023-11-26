88c555fc7bc2b7842fe81e58d572841ddde94408
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2023-11-26 17:01:00 +0530

    Format3

--- common/epoch_logger.py
+++ common/epoch_logger.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.callbacks import Callback
 
+
 class TimeHistory(Callback):
     
     def on_train_begin(self, logs={}):
-        self.start_times : list[str] = []
-        self.end_times : list[str] = []
+        self.start_times: list[str] = []
+        self.end_times: list[str] = []
 
     def on_epoch_begin(self, batch, logs={}):
         self.epoch_time_start = datetime.datetime.now().strftime(r"%H:%M:%S.%f")[:-4]
